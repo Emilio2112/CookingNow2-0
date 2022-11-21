9bfd2384fa15f24ffd5efa35f85f73ecd102160f
Emilio <emilio_casado_de_galdo@hotmail.com>
2022-11-21 18:40:39 +0000

    creación componente header

---
+++ CookinNow/src/components/headerPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <div class="foto"><img src="../assets/images/cookingnow.png" alt="Photo" width="200px" height="200px"></div>
+    </div>
+ </template>
+ 
+ <script >
+ export default {
+    // Lo props tienen que estar en cada componente
+   /* <img src="../assets/images/cookingnow-low-resolution-logo-color-on-transparent-background.png" alt="img" style="width:50%;">*/
+ 
+ }
+ </script>
+ 
+ <style scoped>
+ .header{
+    background-color:  rgb(242,143,75);
+    height: 120px;
+ }
+
+img{
+    margin-top: 10px;
+    margin-left: 20px;
+    width: 130px;
+    height: 100px;
+}
+
+ </style>
