5258cb2c4649c5116c14a92f22bffb1912c3b018
Emilio <emilio_casado_de_galdo@hotmail.com>
2022-11-21 18:39:21 +0000

    Main.js configuración

---
+++ CookinNow/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <headerPage />
+    <RouterLink to="/"></RouterLink>
+    <RouterView />
+    <footerPage />
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import footerPage from "./components/footerPage.vue";
+import headerPage from "./components/headerPage.vue";
+</script>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style scoped></style>
