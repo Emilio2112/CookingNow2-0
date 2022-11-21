7baa8114667dc2b390d299f2ae844ff222cee10a
Emilio <emilio_casado_de_galdo@hotmail.com>
2022-11-21 18:40:19 +0000

    Creación componente footer

---
+++ CookinNow/src/components/footerPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- El nombre que nos muestra en title lo ponemos en App  -->
+    <div class="footer">{{title}} - (c) All rights reserved. 2019</div>
+</template>
+
+<script>
+
+export default {
+    props: {
+    title : String
+  }
+}
+</script>
+
+<style scoped> 
+.footer{
+   background-color:  rgb(242,143,75);
+   width: 100%;
+   font-size: calc(5px + 1vw);
+   text-align: center;
+}
+</style>
