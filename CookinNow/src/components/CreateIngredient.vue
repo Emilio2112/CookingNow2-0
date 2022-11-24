401fa994676c6d7f594a038da8da591baa08a34b
Emilio <emilio_casado_de_galdo@hotmail.com>
2022-11-24 21:00:37 +0000

    CreateIngredient

--- CookinNow/src/components/CreateIngredient.vue
+++ CookinNow/src/components/CreateIngredient.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <label
-      >name
+  <div class="container">
+    <label class="inputIng"
+      >Nombre:
       <input type="text" v-model.trim="newIng.name" />
     </label>
 
-    <button @click.prevent="createIng">Crear Ingrediente</button>
+    <button class="submitBtn" @click.prevent="createIng">Crear Ingrediente</button>
   </div>
 </template>
 
@@ -19,23 +19,75 @@ export default {
   data() {
     return {
       newIng: {
-        name: ""
+        name: "",
       },
       authStore: useAuthStore(),
     };
   },
   methods: {
     async createIng() {
-       this.authStore.isAdmin
-        const response = await API.createIng(this.newIng);
-        if (response.error) {
-          alert("Error creating ingredient");
-        } else {
-          this.$router.push({ name: "homeadmin" });
-        }
+      this.authStore.isAdmin;
+      const response = await API.createIng(this.newIng);
+      if (response.error) {
+        alert("Error creating ingredient");
+      } else {
+        this.$router.push({ name: "homeadmin" });
       }
     },
-  }
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 100px auto;
+  background-color: rgb(242, 143, 75);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 600px;
+  font-family: monospace;
+  font-size: 20px;
+  margin-bottom: 5%;
+  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+}
+
+.submitBtn {
+  margin-left: 5%;
+  border-radius: 7px;
+  font-family: monospace;
+  background-color: #fff;
+  color: rgb(242, 143, 75);
+  border: none;
+  font-size: 20px;
+  transition: all 0.5s;
+  font-weight: bolder;
+  padding: 8px 15px;
+  cursor: pointer;
+}
+
+.submitBtn:active {
+  background-color: rgb(242, 143, 75);
+  transform: translateY(-2px);
+}
+
+.inputIng {
+  margin-top: 2%;
+  margin-bottom: 5%;
+  display: flex;
+  flex-direction: column;
+  margin-right: 2%;
+  margin-left: 2%;
+  font-size: 20px;
+
+}
+
+input {
+  margin-top: 2%;
+  border-radius: 7px;
+  font-family: monospace;
+  font-size: 20px;
+
+}
+</style>
